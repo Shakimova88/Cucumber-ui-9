f5d286957f2ae68587b655a08958bda83437f3ba
Shakimova88 <zarinashakimova@gmail.com>
2024-06-04 13:23:22 -0400

    modified files

--- cypress/pages/TGBasePage.js
+++ cypress/pages/TGBasePage.js
@@ -1,21 +1,21 @@
 class TGBasePage {
-    /* Locators */
-    getLogo() {
-      return cy.get('#logo')
-    }
-  
-    getNavigationDropdown() {
-      return cy.get('#dropdown-container')
-    }
-  
-    getHeading() {
-      return cy.get('#main_heading')
-    }
-  
-    /* Methods */
-    clickDropdownOption(option) {
-      this.getNavigationDropdown().find('a').contains(option).realClick()
-    }
+  /* Locators */
+  getLogo() {
+    return cy.get('#logo')
   }
-  
-  module.exports = TGBasePage
+
+  getNavigationDropdown() {
+    return cy.get('#dropdown-container')
+  }
+
+  getHeading() {
+    return cy.get('#main_heading')
+  }
+
+  /* Methods */
+  clickDropdownOption(option) {
+    this.getNavigationDropdown().find('a').contains(option).realClick()
+  }
+}
+
+module.exports = TGBasePage
